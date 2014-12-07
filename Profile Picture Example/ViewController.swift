552577e494ece9d12d803bdac53aea8e91cfe7c4
louiskonig <louiskonig@gmail.com>
2014-12-07 18:32:49 -0500

    Adding async

--- Profile Picture Example/ViewController.swift
+++ Profile Picture Example/ViewController.swift
@@ -13,15 +13,52 @@ class ViewController: UIViewController {
    
     @IBOutlet weak var imageView: UIImageView!
 
+    
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let myProfilePictureURL = NSURL(string: "http://graph.facebook.com/bobdylan/picture")
-        let imageData = NSData(contentsOfURL: myProfilePictureURL!)
-        self.imageView.image = UIImage(data: imageData!)
+        self.imageView.alpha = 0.0 //Hide imageView until we get the data
         
+        let myProfilePictureURL = NSURL(string: "http://graph.facebook.com/louiskonig/picture?type=square&width=640&height=640")
+        
+
+        
+        let activityIndictorView = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray)
+        activityIndictorView.center = self.view.center
+        self.view.addSubview(activityIndictorView)
+        activityIndictorView.startAnimating()
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            // Do some async stuff
+            //let imageData = NSData(contentsOfURL: myProfilePictureURL!)
+            
+            if let url = myProfilePictureURL? {
+                let imageData = NSData(contentsOfURL: url)
+                if let data = imageData? {
+                    self.imageView.image = UIImage(data: data)
+                }
+            }
+
+            
+            NSOperationQueue.mainQueue().addOperationWithBlock {
+                // Do some main thread stuff
+                //self.imageView.image = UIImage(data: imageData!)
+                activityIndictorView.stopAnimating()
+                
+                UIView.animateWithDuration(1.0, animations: {
+                    self.imageView.alpha = 1.0
+                    }, completion: {
+                        (value: Bool) in
+                        println("Animation complete!")
+                })
+            }
+            
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
